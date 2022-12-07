91d1906138126d815220a085125ee4d67745872c
K0n0D1KuA <3161788646@qq.com>
2022-12-07 20:39:49 +0800

    修改代码

--- src/图算法/TopologicalSorting.java
+++ src/图算法/TopologicalSorting.java
@@ -26,7 +26,7 @@ public class TopologicalSorting {
         //value  : 剩余的入度
         HashMap<Node, Integer> inMap = new HashMap<>();
         //只有剩余入度为0的点才会进入这个队列
-        LinkedList<Node> zeroInQueue = new LinkedList<>();
+        Queue<Node> zeroInQueue = new ArrayDeque<>();
         //初始化 inMap 和 zeroInQueue
         for (Node node : graph.nodes.values()
         ) {
@@ -35,13 +35,13 @@ public class TopologicalSorting {
                 //入队列
                 zeroInQueue.add(node);
                 //排序
-                Collections.sort(zeroInQueue, (o1, o2) -> o2.credit - o1.credit);
+
             }
         }
         //拓扑排序的结果依次放入 result
         ArrayList<Node> result = new ArrayList<>();
         while (!zeroInQueue.isEmpty()) {
-            Collections.sort(zeroInQueue, (o1, o2) -> o2.credit - o1.credit);
+
             Node cur = zeroInQueue.poll();
             result.add(cur);
             //将他们所有连向的地方入度-1
